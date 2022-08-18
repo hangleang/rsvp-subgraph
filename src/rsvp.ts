b27a58ce0beea8274e3d05e0f3d7605e4ed9b25f
leang27 <52003343+leang27@users.noreply.github.com>
2022-08-18 16:53:13 +0700

    build: finished subgraph query on events, attendees, rsvp, confirmedAttendees

--- src/rsvp.ts
+++ src/rsvp.ts
@@ -1,59 +1,116 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { Address, BigInt, ipfs, json } from "@graphprotocol/graph-ts"
 import {
-  RSVP,
   EventCheckedIn,
   EventCreated,
   EventRSVP,
-  UnclaimedDepositPaid
+  UnclaimedDepositPaid,
 } from "../generated/RSVP/RSVP"
-import { ExampleEntity } from "../generated/schema"
+import { Event, Attendee, RSVPOnEvent, AttendedOnEvent } from "../generated/schema"
 
-export function handleEventCheckedIn(event: EventCheckedIn): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+export function handleEventCreated(event: EventCreated): void {
+  let newEvent = Event.load(event.params.eventId.toHex())
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+  if (!newEvent) {
+    newEvent = new Event(event.params.eventId.toHex())
+    newEvent.eventId = event.params.eventId
+    newEvent.creator = event.params.creator
+    newEvent.startDate = event.params.startAt
+    newEvent.deposit = event.params.deposit
+    newEvent.maxAttendees = event.params.maxAttendees.toI32()
+    newEvent.paidOut = false
+    newEvent.unclaimedAmount = BigInt.zero()
+    newEvent.totalRSVPs = 0
+    newEvent.totalConfirmed = 0
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+    let metadata = ipfs.cat(event.params.contentCID + "/data.json")
+    if (metadata) {
+      const value = json.fromBytes(metadata).toObject()
+
+      if (value) {
+        const title = value.get("name")
+        const desc = value.get("description")
+        const link = value.get("link")
+        const imagePath = value.get("image")
+
+        newEvent.title = title ? title.toString() : ''
+        newEvent.description = desc ? desc.toString() : ''
+        newEvent.link = link ? link.toString() : ''
+        
+        if (imagePath) {
+          const imageURL = "https://ipfs.io/ipfs/" + event.params.contentCID + imagePath.toString()
+          newEvent.imageURL = imageURL
+        } else {
+          const fallbackURL =
+            "https://ipfs.io/ipfs/bafybeibssbrlptcefbqfh4vpw2wlmqfj2kgxt3nil4yujxbmdznau3t5wi/event.png";
+          newEvent.imageURL = fallbackURL
+        }
+      }
+    }
+
+    newEvent.save()
   }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.eventId = event.params.eventId
-  entity.attendee = event.params.attendee
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.eventToClaimedRSVPs(...)
-  // - contract.eventToConfirmedRSVPs(...)
-  // - contract.events(...)
 }
 
-export function handleEventCreated(event: EventCreated): void {}
+export function handleEventRSVP(event: EventRSVP): void {
+  const id = event.params.eventId.toHex() + event.params.attendee.toHex()
+  let newRSVP = RSVPOnEvent.load(id)
+  let attendeeAccount = getOrCreateAttendee(event.params.attendee)
+  let targetEvent = Event.load(event.params.eventId.toHex())
 
-export function handleEventRSVP(event: EventRSVP): void {}
+  if (!newRSVP && targetEvent) {
+    newRSVP = new RSVPOnEvent(id)
+    newRSVP.attendee = attendeeAccount.id
+    newRSVP.event = targetEvent.id
+    newRSVP.save()
 
-export function handleUnclaimedDepositPaid(event: UnclaimedDepositPaid): void {}
+    targetEvent.totalRSVPs++
+    targetEvent.save()
+
+    attendeeAccount.totalRSVPs++
+    attendeeAccount.save()
+  }
+}
+
+export function handleEventCheckedIn(event: EventCheckedIn): void {
+  const id = event.params.eventId.toHex() + event.params.attendee.toHex()
+  let newAttended = AttendedOnEvent.load(id)
+  let attendeeAccount = getOrCreateAttendee(event.params.attendee)
+  let targetEvent = Event.load(event.params.eventId.toHex())
+
+  if (!newAttended && targetEvent) {
+    newAttended = new AttendedOnEvent(id)
+    newAttended.attendee = attendeeAccount.id
+    newAttended.event = targetEvent.id
+    newAttended.save()
+
+    targetEvent.totalConfirmed++
+    targetEvent.save()
+
+    attendeeAccount.totalAttendedEvents++
+    attendeeAccount.save()
+  }
+}
+
+export function handleUnclaimedDepositPaid(event: UnclaimedDepositPaid): void {
+  let targetEvent = Event.load(event.params.eventId.toHex())
+
+  if (targetEvent) {
+    targetEvent.paidOut = true
+    targetEvent.unclaimedAmount = event.params.unclaimedAmount
+    targetEvent.save()
+  }
+}
+
+function getOrCreateAttendee(userAddress: Address): Attendee {
+  let attendee = Attendee.load(userAddress.toHex())
+
+  if (!attendee) {
+    attendee = new Attendee(userAddress.toHex())
+    attendee.user = userAddress
+    attendee.totalRSVPs = 0
+    attendee.totalAttendedEvents = 0
+    attendee.save()
+  }
+
+  return attendee
+}
